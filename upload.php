7a3140b1778fb021d70a0a9512f10f812b8750a9
Getulio Sánchez <valentin2507@gmail.com>
2017-11-19 01:25:22 -0500

    Check if time_to_complete_in_days field has valid value

--- upload.php
+++ upload.php
@@ -37,7 +37,7 @@ foreach($tasks as $task) {
     'mentors'=> check_array($task->mentors),
     'is_beginner' => $task->is_beginner,
     'categories' => check_array($task->categories),
-    'time_to_complete_in_days' => $task->time_to_complete_in_days,
+    'time_to_complete_in_days' => check_days_to_complete($task->time_to_complete_in_days),
   ]);
 
   // Sends a request to create the task.
@@ -61,7 +61,7 @@ foreach($tasks as $task) {
  * only one mentor, tag, or category for a task. In such case, this function
  * returns an array version of the value.
  *
- * @param $var
+ * @param array|string|int $var
  *   The value for an array field.
  *
  * @return array
@@ -73,4 +73,26 @@ function check_array($var) {
   }
 
   return $var;
+}
+
+/**
+ * Checks if the time_to_complete_in_days value is valid.
+ * 
+ * GCI API specify that the time to complete in days field should have values
+ * >= 3 and <=7. This function checks if the passed value is in the correct
+ * range.
+ * 
+ * @param int $days_to_complete
+ *   The time to complete the task.
+ * @return int
+ *   A valid value for the time_to_complete_in_day field.
+ */
+function check_days_to_complete($days_to_complete) {
+  if ($days_to_complete < 3) {
+    return 3;
+  } elseif ($days_to_complete > 7) {
+    return 7;
+  } else {
+    return $days_to_complete;
+  }
 }
